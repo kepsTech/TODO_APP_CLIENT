7fa5d846d7e3f209d08f0b00c3a1ffab09218c99
kepsTech <afolabionikepo0@gmail.com>
2023-03-31 00:29:07 +0300

    change backend root url to remote url

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001';
+const BACKEND_ROOT_URL = 'https://todo-backend-1pj7.onrender.com';
 import { Todos } from "./class/Todos.js";
 const todos = new Todos(BACKEND_ROOT_URL);
 const list = document.querySelector('#todolist');
